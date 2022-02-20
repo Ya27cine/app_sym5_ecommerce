eb8db7293883863ee715defedf3ecf93240de3f8
KHELIFA Yassine <prostam27@gmail.com>
2022-02-20 12:34:41 +0100

    Sécuriser la procédure d'ajout d'un produit au panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart/add/{id}", name="cart_add")
+     * @Route("/cart/add/{id}", name="cart_add", requirements={"id" : "\d+"})
      */
-    public function add($id, Request $request): Response
+    public function add($id, Request $request, ProductRepository $productRepository): Response
     {
+
+        // 0. securisation : est-ce que le produit il existe :
+        $product = $productRepository->find($id);
+
+        if(! $product ){
+            throw $this->createNotFoundException("Le produit $id n'existe pas !");
+        }
+
       //dd($request->getSession());
       // 1. retrouver le panier, si n'existe pas alors prendre un tableau vide []
       $cart = $request->getSession()->get('cart', []);
@@ -28,7 +38,9 @@ class CartController extends AbstractController
       // enrigistrer le tableau mis a jour dans la session :
       $request->getSession()->set('cart', $cart);
 
-      dd($request->getSession()->get('cart'));
-
+      return $this->redirectToRoute('product_show',[
+          'category_slug' => $product->getCategory()->getSlug(),
+          'slug' => $product->getSlug()
+      ]);
     }
 }
