98f94f4712ec418ec2de8064b517ba2c06931b98
KHELIFA Yassine <prostam27@gmail.com>
2022-03-22 11:29:26 +0100

    Ajout d composant de mail

--- src/EventDispatcher/ProductViewSubscriber.php
+++ src/EventDispatcher/ProductViewSubscriber.php
@@ -7,14 +7,20 @@ use App\Event\ProductViewEvent;
 use Symfony\Bridge\Monolog\Logger;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 
 class ProductViewSubscriber implements EventSubscriberInterface {
 
     protected $logger;
+    protected $mailer;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
     {
         $this->logger = $logger;
+        $this->mailer = $mailer;
     }
     public static function getSubscribedEvents()
     {
@@ -29,8 +35,20 @@ class ProductViewSubscriber implements EventSubscriberInterface {
 
         $this->product = $event->getProduct();
 
-        $this->logger->info("Email envoye a l admin pour le produit  ".$this->product->getId());
 
+    //     $email = new TemplatedEmail();
+    //     $email->from( new Address("contact@prostam.fr", "Infos de la boutique") )
+    //     ->to("admin@prostam.fr")
+    //     ->subject("Visite du produit n:".$this->product->getId())
+    //     ->htmlTemplate("emails/product_view.html.twig")
+    //     ->context([
+    //             'product' => $this->product
+    //     ]);
+        
+
+    //    $this->mailer->send($email);
+
+        $this->logger->info("Email envoye a l admin pour le produit  ".$this->product->getId());
     }
 
 }
